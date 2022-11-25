42f6ca257fb893bc7e46a935bb81f02b3df905ce
Alex Calderón <alexcalderon@opendeusto.es>
2022-11-25 11:31:25 +0100

    He creado el metodo escribir usuarios

--- ProyectoProgram/src/utilidades/Utilidades.java
+++ ProyectoProgram/src/utilidades/Utilidades.java
@@ -1,8 +1,10 @@
 package utilidades;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -55,5 +57,39 @@ public class Utilidades {
 		}
 		return users;
 	}
+	
+	public static void escribirFihcero(ArrayList<Usuario>users){
+		File aFile = new File("usuarios.txt");
+		
+		try{
+			FileWriter fw = new FileWriter(aFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			for (Usuario usuario : users) {
+				String texto = ""; //creas la variable texto para rellenar en funcion de si es admin o cliente
+				
+				
+				String nombre = usuario.getNombre();
+				int edad = usuario.getEdad();
+				String dNI = usuario.getDNI();
+				String  username = usuario.getUsername();
+				String contrasenya = usuario.getContrasenya();
 
+				texto+= nombre + ";" + edad + ";" + dNI + ";" + username + ";" +contrasenya;
+				
+				if(usuario instanceof Administrador){
+					int antiguedad = ((Administrador) usuario).getAntiguedad();//casteo por que es un metoo del admin
+					texto+= ";" + antiguedad;
+				}
+				bw.write(texto + "\n");
+				
+			}
+			bw.flush();
+			bw.close();
+			fw.close();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
 }
