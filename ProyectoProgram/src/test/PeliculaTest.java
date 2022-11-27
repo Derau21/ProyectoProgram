75848c78e5d4419c5bf72ace2a785fba593d1c18
Pablo De Erauso <pablodeerauso@opendeusto.es>
2022-11-27 18:04:40 +0100

    He creado el test: PeliculaTest y he comprobado todos los metodos de la calse pelicula

---
+++ ProyectoProgram/src/test/PeliculaTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import clases.Pelicula;
+
+
+
+public class PeliculaTest {
+
+	
+	private Pelicula pelicula1;
+	private Pelicula pelicula2;
+	private Pelicula pelicula3;
+	
+	@Before
+	
+	public void setUp() throws Exception{
+	
+		pelicula1= new Pelicula ("Accion", "Piratas Del Caribe", 2);		
+		pelicula2= new Pelicula ("Ficcion", "Stars Wars", 3);		
+		pelicula3= new Pelicula ("Terror", "The Black Phone", 2);		
+	}
+	
+	@Test	
+	
+	public void getGenero() {
+		assertEquals("Accion", pelicula1.getGenero());
+	}
+
+	@Test
+	public void setGenero(String genero) {
+		pelicula1.setGenero("Ficcion");
+		
+		assertEquals("Ficcion", pelicula1.getGenero());
+	}
+	@Test
+	public void getNombre() {
+		assertEquals("Stars Wars", pelicula2.getNombre());
+		
+	}
+	@Test
+	public void setNombre(String nombre) {
+		pelicula2.setNombre("Blancanieves");
+		
+		assertEquals("Blancanieves", pelicula2.getGenero());
+	}
+	
+	@Test
+	public void getDuracion() {
+		assertEquals(2, pelicula3.getDuracion());
+	}
+	@Test
+	public void setDuracion(int duracion) {
+		pelicula3.setDuracion(3);
+		
+		assertEquals(3, pelicula3.getDuracion());
+	}
+		
+}
