5e7a085a2ce3a5a3348b32518daef38acd6def47
Pablo De Erauso <pablodeerauso@opendeusto.es>
2022-11-29 11:01:07 +0100

    He quitado el error de la ventana SeleccionarHorarioYEntradas

--- ProyectoProgram/src/ventanasCliente/SeleccionHorarioYEntradas.java
+++ ProyectoProgram/src/ventanasCliente/SeleccionHorarioYEntradas.java
@@ -12,27 +12,7 @@ public class SeleccionHorarioYEntradas extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SeleccionHorarioYEntradas frame = new SeleccionHorarioYEntradas();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 * @param r 
-	 * @param nuevaReserva 
-	 */
 	public SeleccionHorarioYEntradas(NuevaReserva nuevaReserva, Reserva r) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
