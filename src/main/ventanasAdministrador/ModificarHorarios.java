bfd7cd49fac34dc4c23ab60a5b6378837c79a3d2
alexcalderon17 <114677265+alexcalderon17@users.noreply.github.com>
2022-12-31 12:29:43 +0100

    Errores solucionados

--- src/main/ventanasAdministrador/ModificarHorarios.java
+++ src/main/ventanasAdministrador/ModificarHorarios.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package main.ventanasAdministrador;
 
 import java.awt.EventQueue;
@@ -75,72 +75,3 @@ public class ModificarHorarios extends JFrame {
 	}
 
 }
-=======
-package main.ventanasAdministrador;
-
-import java.awt.Font;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
-
-public class ModificarHorarios extends JFrame {
-
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private General padre;
-
-	
-	public ModificarHorarios(General padre) {
-		this.padre = padre;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 680, 495);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		
-		JLabel lblNewLabel = new JLabel("PELICULAS EN CARTELERA");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setBounds(191, -1, 282, 108);
-		contentPane.add(lblNewLabel);
-		
-		JList list = new JList();
-		list.setBounds(1, 1, 665, 468);
-		contentPane.add(list);
-		
-		JLabel lblNewLabel_1 = new JLabel("Selecciona la pelicula que quieres modificar:");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel_1.setBounds(26, 89, 282, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JButton btnAtras = new JButton("ATRAS");
-		btnAtras.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnAtras.setBounds(518, 392, 118, 39);
-		contentPane.add(btnAtras);
-		
-		JButton btnEliminar = new JButton("MODIFICAR");
-		btnEliminar.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnEliminar.setBounds(159, 383, 229, 64);
-		contentPane.add(btnEliminar);
-		
-		JScrollPane scrollPane = new JScrollPane(list);
-		scrollPane.setBounds(36, 118, 500, 185);
-		contentPane.add(scrollPane);
-		
-		JLabel lblNewLabel_2 = new JLabel("Nuevo horario:");
-		lblNewLabel_2.setBounds(36, 332, 145, 29);
-		contentPane.add(lblNewLabel_2);
-		
-		
-
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/Derau21/ProyectoProgram.git
